e2e8269484b748c74f104f10514608f5613af9ef
mintunmedia <31453740+mintunmedia@users.noreply.github.com>
2018-10-10 16:57:38 -0700

    Update includes/show_help_window.php

--- includes/show_help_window.php
+++ includes/show_help_window.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
@@ -8,11 +8,10 @@ $group_id = get_option("mm_custom_field_group_id");?>
 <div id="group_popup_container">
 	<h2>
 		<span class="group_title">Groups for MemberMouse Help</span>
-		<span class="group_close"><a href="javascript:MGROUP.closeGroupPopup();" title="Close">Close</a></span>	
+		<span class="group_close"><a href="javascript:MGROUP.closeGroupPopup();" title="Close">Close</a></span>
 	</h2>
 	<div id="group_popup_main">
-		<p><a href="https://membermouseplus.com/support" target="_blank" class="mm-ui-button blue">Contact Support</a></p>
-		<p><a href="http://support.membermouse.com/knowledgebase/articles/319238-groups-extension-overview-beta" target="_blank" class="mm-ui-button blue">Documentation</a></p>
+		<p><a href="https://www.powerpackformembermouse.com/support" target="_blank" class="mm-ui-button blue">Contact Support</a></p>
 		<p style="margin-top:25px;">Add the following SmartTag to your checkout page within the <code>[MM_Form type="checkout"]</code> and <code>[/MM_Form]</code> tags: </p>
 		<p><input id="mm-group-id-custom-field" type="text" readonly value="<?php echo "[MM_Form_Field type='custom-hidden' id='{$group_id}']"; ?>" style="width:320px; font-family:courier; font-size:11px;" onclick="jQuery('#mm-group-id-custom-field').focus(); jQuery('#mm-group-id-custom-field').select();" /></p>
 		<p style="margin-top:25px;">Use the following SmartTag to display the group signup link to Group leaders: </p>
