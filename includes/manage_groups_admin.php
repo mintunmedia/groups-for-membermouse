274b977e4cd49c27b798b9202ebaaa3fd72e3b78
Matt Mintun <36771702+mattmintun@users.noreply.github.com>
2021-04-05 15:44:40 -0700

    add expired status to group leader changed

--- includes/manage_groups_admin.php
+++ includes/manage_groups_admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Admin Dashboard to Manage Specific Groups
  * Allows Admin to:
@@ -8,24 +9,23 @@
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-include_once( WP_PLUGIN_DIR . "/membermouse/includes/mm-constants.php" );
-include_once( WP_PLUGIN_DIR . "/membermouse/includes/init.php" );
+include_once(WP_PLUGIN_DIR . "/membermouse/includes/mm-constants.php");
+include_once(WP_PLUGIN_DIR . "/membermouse/includes/init.php");
 
 global $wpdb, $current_user;
 
 // Get Group Leader ID
-error_log("Group Leader: ". $_GET['group_leader']);
-if ( isset($_GET['group_leader']) && ! empty($_GET['group_leader']) && is_numeric($_GET['group_leader']) ) {
+if (isset($_GET['group_leader']) && !empty($_GET['group_leader']) && is_numeric($_GET['group_leader'])) {
 	$group_leader_id = $_GET['group_leader'];
 } else {
 	// No Group Leader ID. Redirect back
-	?>
+?>
 	<script type="text/javascript">
 		window.location = 'admin.php?page=groupsformm&type=manage';
 	</script>
-	<?php
+<?php
 }
 
 ?>
@@ -34,45 +34,45 @@ if ( isset($_GET['group_leader']) && ! empty($_GET['group_leader']) && is_numeri
 	<div id="create_group_content" style="display:none;"></div>
 </div>
 <div id="group_popup_msg">
-	<?php if ( isset( $_GET["delete"] ) ) {
-		if ( $_GET["delete"] == 1 ) { ?>
+	<?php if (isset($_GET["delete"])) {
+		if ($_GET["delete"] == 1) { ?>
 			<div class="group_success">The operation completed successfully.</div>
 		<?php } elseif ($_GET["delete"] == 0) { ?>
 			<div class="group_failure">An error occured. Please try again later.</div>
 		<?php }
 	}
 
-	if ( isset( $_GET["ndelete"] ) ) {
-		if ($_GET["ndelete"] == 1 ) { ?>
+	if (isset($_GET["ndelete"])) {
+		if ($_GET["ndelete"] == 1) { ?>
 			<div class="group_success">Successfully deleted the notice.</div>
-		<?php } elseif ( $_GET["ndelete"] == 0 ) { ?>
+		<?php } elseif ($_GET["ndelete"] == 0) { ?>
 			<div class="group_failure">An error occured. Please try again later.</div>
-		<?php }
+	<?php }
 	} ?>
 </div>
 <?php
-if ( isset( $_GET['notice'] ) && ! empty( $_GET['notice'] ) && is_numeric( $_GET['notice'] ) ) {
+if (isset($_GET['notice']) && !empty($_GET['notice']) && is_numeric($_GET['notice'])) {
 	$notice 	= $_GET['notice'];
-	$delSql		= "DELETE FROM " . $wpdb->prefix . "group_notices WHERE id = '". $notice ."'";
-	$delQuery	= $wpdb -> query($delSql);
-	if ( $delQuery ) { ?>
+	$delSql		= "DELETE FROM " . $wpdb->prefix . "group_notices WHERE id = '" . $notice . "'";
+	$delQuery	= $wpdb->query($delSql);
+	if ($delQuery) { ?>
 		<script type="text/javascript">
 			window.location = 'admin.php?page=groupsformm&type=manage&ndelete=1';
 		</script>
-<?php } else { ?>
+	<?php } else { ?>
 		<script type="text/javascript">
 			window.location = 'admin.php?page=groupsformm&type=manage&ndelete=0';
 		</script>
-<?php
+	<?php
 	}
 }
 
 /**
  * Get Group ID from DB
  */
-$sql				= "SELECT id, group_name FROM ". $wpdb->prefix ."group_sets WHERE group_leader = '". $group_leader_id ."'";
+$sql				= "SELECT id, group_name FROM " . $wpdb->prefix . "group_sets WHERE group_leader = '" . $group_leader_id . "'";
 $result 		= $wpdb->get_row($sql);
-if ( $result ) {
+if ($result) {
 	$gid				= $result->id;
 	$group_name = $result->group_name;
 } else {
@@ -80,21 +80,21 @@ if ( $result ) {
 	<script type="text/javascript">
 		window.location = 'admin.php?page=groupsformm&type=manage';
 	</script>
-	<?php
+<?php
 }
 
 // Get total results
-$totalSql	= "SELECT COUNT(id) AS total FROM ". $wpdb->prefix ."group_sets_members WHERE group_id = '". $gid ."'";
+$totalSql	= "SELECT COUNT(id) AS total FROM " . $wpdb->prefix . "group_sets_members WHERE group_id = '" . $gid . "'";
 $totalRes	= $wpdb->get_row($totalSql);
 $members  = $totalRes->total;
 
 // Show # Results Handler
 $show	= 0;
-if ( isset($_GET["show"] ) && !empty( $_GET["show"] ) && is_numeric( $_GET['show'] ) ) {
+if (isset($_GET["show"]) && !empty($_GET["show"]) && is_numeric($_GET['show'])) {
 	$show = $_GET["show"];
 }
 
-if ( ! empty( $show ) ) {
+if (!empty($show)) {
 	$limit = $show;
 } else {
 	$limit = 10;
@@ -102,45 +102,45 @@ if ( ! empty( $show ) ) {
 
 // Page # Handler
 $page = 0;
-if ( isset( $_GET['p'] ) && ! empty( $_GET['p'] ) && is_numeric( $_GET['p'] ) ) {
+if (isset($_GET['p']) && !empty($_GET['p']) && is_numeric($_GET['p'])) {
 	$page 	= $_GET['p'];
-	$start 	= ( $page - 1 ) * $limit;
+	$start 	= ($page - 1) * $limit;
 } else {
 	$start	= 0;
 }
 
-if( $page == 0 ) {
+if ($page == 0) {
 	$page = 1;
 }
 
 // Perform Actual SQL to pull results with limit and page
-$gMemSql = "SELECT * FROM ". $wpdb->prefix ."group_sets_members WHERE group_id = '". $gid ."' ORDER BY createdDate DESC LIMIT $start, $limit";
+$gMemSql = "SELECT * FROM " . $wpdb->prefix . "group_sets_members WHERE group_id = '" . $gid . "' ORDER BY createdDate DESC LIMIT $start, $limit";
 $gMemResults	= $wpdb->get_results($gMemSql);
 
 
 // Set Target Page
-$targetpage = 'admin.php?page=groupsformm&type=manage_group&group_leader='. $group_leader_id;
-if ( ! empty( $show ) ) {
+$targetpage = 'admin.php?page=groupsformm&type=manage_group&group_leader=' . $group_leader_id;
+if (!empty($show)) {
 	$targetpage .= '&show=' . $show;
 }
 ?>
 
 <h2><em><?php echo $group_name; ?></em> Management Dashboard</h2>
 <div class="membermousegroupbuttoncontainer">
-	<a class="group-button button-green button-small" title="Edit Group Name" id="edit_group" onclick="javascript:MGROUP.editGroupNameForm('<?php echo $gid;?>','<?= $group_leader_id; ?>');">
+	<a class="group-button button-green button-small" title="Edit Group Name" id="edit_group" onclick="javascript:MGROUP.editGroupNameForm('<?php echo $gid; ?>','<?= $group_leader_id; ?>');">
 		Edit Group Name
 	</a>&nbsp;&nbsp;
-	<a class="group-button button-green button-small" title="Signup Link" id="purchase_link" onclick="javascript:MGROUP.showMemberPurchaseLink('<?php echo $gid;?>', '<?= $group_leader_id; ?>');">
+	<a class="group-button button-green button-small" title="Signup Link" id="purchase_link" onclick="javascript:MGROUP.showMemberPurchaseLink('<?php echo $gid; ?>', '<?= $group_leader_id; ?>');">
 		Signup Link
 	</a>
 </div>
 <div class="clear"></div>
 
-<?php if( $members == 0 ) { ?>
+<?php if ($members == 0) { ?>
 	<p><em>No members found.</em></p>
 <?php } else {
 
-	echo MemberMouseGroupAddon::MemberMouseGroupPagination($limit, $members, $page, $start, $targetpage, 'members');?>
+	echo MemberMouseGroupAddon::MemberMouseGroupPagination($limit, $members, $page, $start, $targetpage, 'members'); ?>
 	<table class="widefat" id="mm-data-grid" style="width:96%">
 		<thead>
 			<tr>
@@ -152,65 +152,65 @@ if ( ! empty( $show ) ) {
 			</tr>
 		</thead>
 		<tbody>
-      <?php	foreach($gMemResults as $gMemRes):
-				$userSql		  = "SELECT * FROM ". $wpdb->prefix ."users WHERE ID = '". $gMemRes->member_id ."'";
+			<?php foreach ($gMemResults as $gMemRes) :
+				$userSql		  = "SELECT * FROM " . $wpdb->prefix . "users WHERE ID = '" . $gMemRes->member_id . "'";
 				$userResult		= $wpdb->get_row($userSql);
 				$registered		= $userResult->user_registered;
-				$memSql			  = "SELECT * FROM mm_user_data WHERE wp_user_id = '". $gMemRes->member_id ."'";
+				$memSql			  = "SELECT * FROM mm_user_data WHERE wp_user_id = '" . $gMemRes->member_id . "'";
 				$memResult		= $wpdb->get_row($memSql);
 				$firstName 		= $memResult->first_name;
 				$lastName 		= $memResult->last_name;
 				$email 			  = $userResult->user_email;
 				$statusId 		= $memResult->status;
 				$membershipId	= $memResult->membership_level_id;
-				$levelSql 		= "SELECT name FROM mm_membership_levels WHERE id = '". $membershipId ."'";
+				$levelSql 		= "SELECT name FROM mm_membership_levels WHERE id = '" . $membershipId . "'";
 				$levelResult	= $wpdb->get_row($levelSql);
 				$membershipName	= $levelResult->name;
 				$redirecturl  = "";
 				$crntMemberId = $gMemRes->member_id;
 				$member		 		= new MM_User($crntMemberId);
-				$url 				  = "javascript:mmjs.changeMembershipStatus('". $crntMemberId ."', '". $membershipId ."', ". MM_Status::$CANCELED .", '" .$redirecturl ."')";
-				$cancellationHtml = "<a title=\"Cancel Member\" style=\"cursor: pointer;display: none;\" onclick=\"".$url."\"/>". MM_Utils::getIcon('stop', 'red', '1.2em', '1px') ."</a>";
+				$url 				  = "javascript:mmjs.changeMembershipStatus('" . $crntMemberId . "', '" . $membershipId . "', " . MM_Status::$CANCELED . ", '" . $redirecturl . "')";
+				$cancellationHtml = "<a title=\"Cancel Member\" style=\"cursor: pointer;display: none;\" onclick=\"" . $url . "\"/>" . MM_Utils::getIcon('stop', 'red', '1.2em', '1px') . "</a>";
 
 				// Get Member's Active Subscriptions - includes overdue subscriptions
 				$activeSubscriptions = $member->getActiveMembershipSubscriptions(true);
 
-				if ( empty($activeSubscriptions) ) {
+				if (empty($activeSubscriptions)) {
 					// No Subscriptions
 					$has_subscriptions = false;
 				} else {
 					$has_subscriptions = true;
 				}
 
-				?>
+			?>
 				<tr>
-					<td><?= $firstName.'&nbsp;'.$lastName;?></td>
-					<td><a href="admin.php?page=manage_members&module=details_general&user_id=<?= $crntMemberId; ?>" target="_blank"><?= $email;?></a></td>
-					<td><?= date('F d, Y h:m a',strtotime($registered));?></td>
+					<td><?= $firstName . '&nbsp;' . $lastName; ?></td>
+					<td><a href="admin.php?page=manage_members&module=details_general&user_id=<?= $crntMemberId; ?>" target="_blank"><?= $email; ?></a></td>
+					<td><?= date('F d, Y h:m a', strtotime($registered)); ?></td>
 					<td><?= MM_Status::getImage($statusId); ?></td>
 					<td>
 						<?php
-						if ( $has_subscriptions ) {
+						if ($has_subscriptions) {
 							// Member has active subscriptions. Show error
 							echo $cancellationHtml;
 							echo MM_Utils::getDeleteIcon("This member has an active paid membership which must be canceled before they can be removed from the group", 'margin-left:5px;', '', true);
 						} else {
-							$deleteActionUrl = 'onclick="javascript:MGROUP.deleteGroupMember('. $gMemRes->id .','. $gMemRes->member_id .');"';
+							$deleteActionUrl = 'onclick="javascript:MGROUP.deleteGroupMember(' . $gMemRes->id . ',' . $gMemRes->member_id . ');"';
 							echo MM_Utils::getDeleteIcon("Remove the member from this group", 'margin-left:5px;', $deleteActionUrl);
 						}
 						?>
 					</td>
 				</tr>
-      <?php	endforeach;?>
+			<?php endforeach; ?>
 		</tbody>
 	</table>
 <?php }
 
-$noticeSql		 = "SELECT * FROM ". $wpdb->prefix ."group_notices WHERE msg_type = '1' AND leader_id = '". $group_leader_id ."' ORDER BY createdDate DESC";
+$noticeSql		 = "SELECT * FROM " . $wpdb->prefix . "group_notices WHERE msg_type = '1' AND leader_id = '" . $group_leader_id . "' ORDER BY createdDate DESC";
 $noticeResults = $wpdb->get_results($noticeSql);
 $noticeCount	 = count($noticeResults);
 
-if($noticeCount > 0) : ?>
+if ($noticeCount > 0) : ?>
 	<div class="group_notices">
 		<h3>Notices</h3>
 		<table class="widefat" id="mm-data-grid">
@@ -221,28 +221,28 @@ if($noticeCount > 0) : ?>
 				</tr>
 			</thead>
 			<tbody>
-<?php		foreach($noticeResults as $noticeResult):
-          $groupSql		= "SELECT group_name FROM ".$wpdb -> prefix."group_sets WHERE id = '".$noticeResult -> group_id."'";
-          $groupResult	= $wpdb -> get_row($groupSql);
-          $groupName		= $groupResult -> group_name;
+				<?php foreach ($noticeResults as $noticeResult) :
+					$groupSql		= "SELECT group_name FROM " . $wpdb->prefix . "group_sets WHERE id = '" . $noticeResult->group_id . "'";
+					$groupResult	= $wpdb->get_row($groupSql);
+					$groupName		= $groupResult->group_name;
 
-          $userSql		= "SELECT user_email FROM ".$wpdb -> prefix."users WHERE ID = '".$noticeResult -> user_id."'";
-          $userResult		= $wpdb -> get_row($userSql);
-          $userEmail		= $userResult -> user_email;
+					$userSql		= "SELECT user_email FROM " . $wpdb->prefix . "users WHERE ID = '" . $noticeResult->user_id . "'";
+					$userResult		= $wpdb->get_row($userSql);
+					$userEmail		= $userResult->user_email;
 
-          $leaderSql		= "SELECT user_email FROM ".$wpdb -> prefix."users WHERE ID = '".$noticeResult -> leader_id."'";
-          $leaderResult	= $wpdb -> get_row($leaderSql);
-          $leaderEmail	= $leaderResult -> user_email;
-?>
-          <tr>
-            <td>Member <span style="color:#FF0000;"><?php echo $userEmail;?></span> failed to join <?php echo $groupName;?> (<?php echo $leaderEmail;?>) because it was full. Please cancel that member account and inform the group leader.</td>
-            <td>
-              <a title="Delete Notice" href="admin.php?page=groupsformm&type=manage&notice=<?php echo $noticeResult -> id;?>">
-                Delete Notice
-              </a>
-            </td>
-          </tr>
-<?php		endforeach;?>
+					$leaderSql		= "SELECT user_email FROM " . $wpdb->prefix . "users WHERE ID = '" . $noticeResult->leader_id . "'";
+					$leaderResult	= $wpdb->get_row($leaderSql);
+					$leaderEmail	= $leaderResult->user_email;
+				?>
+					<tr>
+						<td>Member <span style="color:#FF0000;"><?php echo $userEmail; ?></span> failed to join <?php echo $groupName; ?> (<?php echo $leaderEmail; ?>) because it was full. Please cancel that member account and inform the group leader.</td>
+						<td>
+							<a title="Delete Notice" href="admin.php?page=groupsformm&type=manage&notice=<?php echo $noticeResult->id; ?>">
+								Delete Notice
+							</a>
+						</td>
+					</tr>
+				<?php endforeach; ?>
 			</tbody>
 		</table>
 	</div>
